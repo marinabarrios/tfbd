4f46a63e459e7f9cf1e841438af9ab93d81a95d8
marinabarrios <marina_paolab@hotmail.com>
2024-10-05 19:31:46 -0300

    subo el arranque del proyecto con una bd relacional

---
+++ src/main/java/com/example/tf/TfApplication.java
@@ -0,0 +1,13 @@
+package com.example.tf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tf/TfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
